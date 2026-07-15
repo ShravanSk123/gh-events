5d1da763e46db4e5eee77b1f6843409b53ae2ede
Shravan <sk.shravan00@gmail.com>
2026-07-15 11:06:03 +0530

    Add a comment in HelpBox component to test workflow [skip ci]

--- src/components/HelpBox.jsx
+++ src/components/HelpBox.jsx
@@ -4,6 +4,7 @@ import './HelpBox.css';
 
 function HelpBox({ title, text }) {
   return (
+    //Random comment to test the workflow
     <article className="help-box">
       <h3>{title}</h3>
       <p>{text}</p>
